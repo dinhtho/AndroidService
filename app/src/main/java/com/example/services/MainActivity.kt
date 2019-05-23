0ecc7abfcc7bf4e3912bcde79cc86bb1e96e2dcc
thonguyen <dinhthohcmus@gmail.com>
2019-05-23 18:44:40 +0700

    update code

--- app/src/main/java/com/example/services/MainActivity.kt
+++ app/src/main/java/com/example/services/MainActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import android.content.Intent
 import android.os.Build
 import android.annotation.TargetApi
+import android.content.ComponentName
+import android.content.Context
+import android.content.ServiceConnection
+import android.os.IBinder
 import android.support.v4.app.FragmentActivity
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,6 +22,18 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, MyService::class.java)
         bt_start.setOnClickListener { startService(intent) }
         bt_stop.setOnClickListener { stopService(intent) }
+        bt_bind.setOnClickListener { bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE) }
+        bt_unbind.setOnClickListener { unbindService(serviceConnection) }
+    }
+
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
     }
 //
 //    @TargetApi(Build.VERSION_CODES.O)
